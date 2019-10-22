b58204f18a1ed6cce75491ab96abcb9909a2744e
Lucas Bazemore <lucasbazemore@Lucass-MacBook-Pro.local>
2019-10-22 03:21:54 -0500

    update seeds with more examples

--- data/seeds/03_snacks.js
+++ data/seeds/03_snacks.js
@@ -34,11 +34,46 @@ exports.seed = function(knex) {
                         'https://images-na.ssl-images-amazon.com/images/I/51615qZv50L.jpg',
                 },
                 {
-                    name: 'Pepper Jack Chee',
-                    brand: 'Pepperidge Farms',
-                    uom: '30 oz box',
+                    name: 'Pepper Jack Cheez-It',
+                    brand: 'Sunshine',
+                    uom: '48 oz box',
                     img_url:
-                        'https://images-na.ssl-images-amazon.com/images/I/51615qZv50L.jpg',
+                        'https://s7d6.scene7.com/is/image/bjs/266604?$bjs-Zoom$',
+                },
+                {
+                    name: 'Movie Theater Butter Popcorn',
+                    brand: 'Orville Redenbacher',
+                    uom: '12, 3 oz bags',
+                    img_url:
+                        'https://i5.walmartimages.com/asr/f3d3ee48-61b9-48f1-9642-93c3d113f96c_1.a9a8a235fefd1dc50f613aed9b66778e.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff',
+                },
+                {
+                    name: 'Pretzels',
+                    brand: 'Utz',
+                    uom: '60 oz container',
+                    img_url:
+                        'https://i5.walmartimages.com/asr/85edf0bf-47b9-4efe-b80e-763471331418_1.a76a799e184d45f79b649ee95b1cfa43.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff',
+                },
+                {
+                    name: 'Slim Jims',
+                    brand: 'Conagra',
+                    uom: '46 8g sticks',
+                    img_url:
+                        'https://images-na.ssl-images-amazon.com/images/I/91vFHKbAnKL._SL1500_.jpg',
+                },
+                {
+                    name: 'Rice Krispies Treats',
+                    brand: 'Kellogs',
+                    uom: '16, 22g bars',
+                    img_url:
+                        'https://images-na.ssl-images-amazon.com/images/I/91Ys2CURhtL._SL1500_.jpg',
+                },
+                {
+                    name: 'Chewy | Dipped Chewy',
+                    brand: 'Quaker Oats',
+                    uom: '58 bars',
+                    img_url:
+                        'https://images-na.ssl-images-amazon.com/images/I/91zqi-KptOL._SL1500_.jpg',
                 },
             ]);
         });
